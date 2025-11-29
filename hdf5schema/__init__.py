bff6119dd3b1054ad67025d6d319948d2e0ea09c
Stephen Hartzell <hartzell.stephen@gmail.com>
2025-11-29 09:13:59 -0500

    Updated version number and changelog

--- hdf5schema/__init__.py
+++ hdf5schema/__init__.py
@@ -2,7 +2,7 @@
 HDF5 Schema - HDF5 Schema is Python library for defining schemas for HDF5 files and verifying compliance.
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "Stephen Hartzell"
 __email__ = "hartzell.stephen@gmail.com"
 
